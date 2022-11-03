5c159277c77619057846b00e75adbbc8daf175e8
oviozz <42685801+oviozz@users.noreply.github.com>
2022-11-02 23:55:00 -0700

    Update login_fillin.py

--- login_fillin.py
+++ login_fillin.py
@@ -1,12 +1,11 @@
-import os
 
 class RegisterCheck:
-    os.system("attrib +h sign_up_form.txt")
 
     def signup_checking(self, name, email, password, error_handle, switch):
         if len(name.text()) > 3 and len(email.text()) > 3 and len(password.text()) > 3:
             with open("sign_up_form.txt", "a+") as store:
                 store.write(f'{name.text().lower()}:{password.text().lower()}\n')
+
                 return switch.setCurrentIndex(switch.currentIndex() + 1)
         return error_handle.setText('Please fill out the info')
 
@@ -19,11 +18,15 @@ class RegisterCheck:
 class PasswordCheck:
 
     def login_check(self, name, password, error_handle):
-        with open('sign_up_form.txt', 'r') as store:
-            for user in store.readlines():
-                if name.lower() == user.split(':')[0] and password.lower() == user.replace('\n', '').split(':')[1]:
-                    return True
-        error_handle.setText('Incorrect User ID')
+        try:
+            with open('sign_up_form.txt', 'r') as store:
+                for user in store.readlines():
+                    if name.lower() == user.split(':')[0] and password.lower() == user.replace('\n', '').split(':')[1]:
+                        return True
+            error_handle.setText('Incorrect User ID')
+            
+        except:
+            return error_handle.setText('Incorrect User ID')
         return False
 
 
